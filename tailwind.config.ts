4d232b090fffef8bc1168153be1d9ece49253daf
DeyanTopalov <deyan.topalow@gmail.com>
2024-04-28 21:20:55 +0300

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,14 @@
 import type { Config } from "tailwindcss";
 
+function withOpacity(variableName: string) {
+  return ({ opacityValue }: { opacityValue?: number }): string => {
+    if (opacityValue !== undefined) {
+      return `hsla(var(${variableName}), ${opacityValue})`;
+    }
+    return `hsla(var(${variableName}))`;
+  };
+}
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,6 +17,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "clr-light-cyan": withOpacity("--light-cyan")({}),
+        "clr-neon-green": withOpacity("--neon-green")({}),
+        "clr-grayish-400": withOpacity("--grayish-400")({}),
+        "clr-grayish-600": withOpacity("--grayish-600")({}),
+        "clr-grayish-800": withOpacity("--grayish-800")({}),
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
